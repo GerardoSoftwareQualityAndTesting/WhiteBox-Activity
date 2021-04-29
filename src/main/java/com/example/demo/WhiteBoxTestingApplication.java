d0760e8724b834416a2962df4b7d8808019b124d
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-04-28 21:32:21 -0600

    initial commit

---
+++ src/main/java/com/example/demo/WhiteBoxTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBoxTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBoxTestingApplication.class, args);
+	}
+
+}
